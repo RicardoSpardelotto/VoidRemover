0d058a2b7c63acd79139b3bf0019a7912fb0cd73
RicardoSpardelotto <63017205+RicardoSpardelotto@users.noreply.github.com>
2022-09-07 19:07:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Void-Remover
+ Removedor de plano de fundo de imagens desenvolvido por Ricardo Spardelotto e Luan Carlos Saretta da Rosa
---
+++ android/app/src/main/kotlin/com/example/voidremover/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.voidremover
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
